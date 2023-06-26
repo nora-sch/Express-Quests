af2e8b993160719159861ee6ba780ad9f903f40e
nora-sch <nora.schumann.development@gmail.com>
2023-06-26 15:52:19 +0200

    add verifyToken step

--- auth.js
+++ auth.js
@@ -33,7 +33,7 @@ const verifyPassword = (req, res) => {
           expiresIn: "1h",
         });
         delete req.user.hashedPassword;
-        res.send({token, user:req.user});
+        res.send({ token, user: req.user });
       } else {
         res.sendStatus(401);
       }
@@ -45,22 +45,24 @@ const verifyPassword = (req, res) => {
 };
 
 const verifyToken = (req, res, next) => {
-
   try {
-
+    const authorizationHeader = req.get("Authorization");
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+    const [type, token] = authorizationHeader.split(" ");
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
     next();
-
   } catch (err) {
-
     console.error(err);
-
     res.sendStatus(401);
-
   }
-
 };
 module.exports = {
   hashPassword,
   verifyPassword,
-  verifyToken
+  verifyToken,
 };
